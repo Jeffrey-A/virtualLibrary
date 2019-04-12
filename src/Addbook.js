f066e5ec633ed5f5800c97f8b99febd0667e8d80
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-04-12 09:40:06 -0400

    info from the addBook container grabed

--- src/Addbook.js
+++ src/Addbook.js
@@ -5,8 +5,9 @@ const close = () =>{
     document.getElementById('addBook').style.display="none";
 }
 
-const addBook = () =>{
+const getBookInfo = (info) =>{
     //add a book component
+    alert(info.title +" "+info.author + " "+info.image);
 }
 
 class BookInfo extends Component{
@@ -16,17 +17,22 @@ class BookInfo extends Component{
                 <span id="closeBookAdding" onClick={close}>X</span>
                 <h1>Add Book</h1>
                 <p>Title: </p>
-                <input type="text" placeholder="Book's title"/>
+                <input id="titleProvide"type="text" placeholder="Book's title"/>
 
                 <p>Author: </p>
-                <input type="text" placeholder="Book's Author"/>
+                <input id="authorProvide" type="text" placeholder="Book's Author"/>
             
                 <p>Book's image: </p>
-                <input type="text" placeholder="img url"/>
+                <input id="imgProvide"type="text" placeholder="img url"/>
 
-                <button id="addButton" onClick={addBook}>ADD</button>
+                <button id="addButton" onClick={()=>getBookInfo(
+                    {
+                        title:document.getElementById("titleProvide").value,
+                        author:document.getElementById("authorProvide").value,
+                        image:document.getElementById("imgProvide").value
 
-                
+                    }
+                )}>ADD</button>  
             </div>
         );
     }
--- src/Nav.js
+++ src/Nav.js
@@ -6,20 +6,14 @@ import BookInfo from './Addbook';
 const addBook = () =>{
     const container = document.getElementById('addBook');
     container.style.display = "block";
-    ReactDOM.render(
-        <BookInfo />,
-        container
-    )
-    
+    ReactDOM.render(<BookInfo />,container); 
 }
 
 
 const Nav = () =>{
     return (
         <nav className="navigation-bar text-center">
-            <button type="button" onClick={addBook}>
-            Add Book
-            </button>
+            <button type="button" onClick={addBook}>Add Book</button>
             <input type="text" placeholder="Search books" />
             <Button className="search-bt" text="Search" />
             
