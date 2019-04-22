708f7cac070eea77032b3dd723df1d4ad57acef9
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-04-22 18:25:45 -0400

    google fonts added

--- js/script.js
+++ js/script.js
@@ -2,23 +2,24 @@ const input = $("#input-box");
 const searchBt = $("#search");
 
 const getBooks = ()=>{
-    if(input.val() !=""){
-        $(".books-container").empty();
-        $("#info").empty();
+    if(input.val() !=""){ //if input text is not empty
+        $(".books-container").empty(); //remove all books
+        $("#info").empty(); // remove books info
 
         $.get("https://www.googleapis.com/books/v1/volumes?q="+input.val()+"&maxResults=40&key=AIzaSyCCTinyvw6cYu4Fengg7zhPvBOUEZIuW9Q",
-        (response)=>{
+        (response)=>{ //make a request to the google books API
 
             const inputText = input.val();
             input.val("");
-            if(response.totalItems <=0){
+            if(response.totalItems <=0){ //if did not get any book 
                 $('#info').text("No "+inputText+" books found in google books.")
-            }else{
+            }else{ //fetched books correctly
                
                 const booksContainer = $(".books-container");
                 const books =[];
                 let items = response.items;
 
+                //in case a book does not have an author 
                 for(var i=0; i < items.length; i++){
                     if(!(items[i].volumeInfo.authors)){
                         items[i].volumeInfo.authors=["unknown"]
@@ -29,7 +30,7 @@ const getBooks = ()=>{
                         items[i].volumeInfo.imageLinks.thumbnail="../placeholder.png";
                     }
                 }
-
+                //pushing all books to the list
                 for(var i=0; i < items.length; i++){
                     books.push(
                         {
@@ -43,11 +44,13 @@ const getBooks = ()=>{
                     )
                 }
 
+                //info text
                 let first = inputText[0].toUpperCase();
                 let info = $("<p></p>").text(first+inputText.substring(1,inputText.length)+" books");
                 info.addClass('book-info');
                 $("#info").append(info);
 
+                //constructing book cards and adding them to booksContainer
                 for(var i=0; i < books.length; i++){
                     let title = $("<p></p>").text(books[i].title);
                     title.addClass("book-title")
@@ -76,6 +79,7 @@ const getBooks = ()=>{
     }
 }
 
+//Events
 input.on('keypress', (e)=>{
     if(e.keyCode=='13'){
         getBooks();
