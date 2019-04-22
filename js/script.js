2749302689a4757cae6256f216a25fd29e7d65e2
Jeffrey Almanzar <almanzarortizjeffrey@gmail.com>
2019-04-21 21:56:30 -0400

    app styled

--- js/script.js
+++ js/script.js
@@ -4,59 +4,73 @@ const searchBt = $("#search");
 const getBooks = ()=>{
     if(input.val() !=""){
         $(".books-container").empty();
+        $("#info").empty();
 
-        $.get("https://www.googleapis.com/books/v1/volumes?q="+input.val()+"+intitle:keyes&key=AIzaSyCCTinyvw6cYu4Fengg7zhPvBOUEZIuW9Q",
+        $.get("https://www.googleapis.com/books/v1/volumes?q="+input.val()+"&key=AIzaSyCCTinyvw6cYu4Fengg7zhPvBOUEZIuW9Q",
         (response)=>{
-            const booksContainer = $(".books-container");
-            const books =[];
-            let items = response.items;
 
-            for(var i=0; i < items.length; i++){
-                if(!(items[i].volumeInfo.authors)){
-                    items[i].volumeInfo.authors=["unknown"]
-                }
+            const inputText = input.val();
+            input.val("");
+            if(response.totalItems <=0){
+                $('#info').text("No "+inputText+" books found in google books.")
+            }else{
+               
+                const booksContainer = $(".books-container");
+                const books =[];
+                let items = response.items;
 
-                if(!(items[i].volumeInfo.imageLinks)){
-                    items[i].volumeInfo.imageLinks={};
-                    items[i].volumeInfo.imageLinks.thumbnail="../placeholder.png";
-                }
-            }
-
-            for(var i=0; i < items.length; i++){
-                books.push(
-                    {
-                        title: items[i].volumeInfo.title,
-                        author: items[i].volumeInfo.authors[0],
-                        img: items[i].volumeInfo.imageLinks.thumbnail,
-                        description: items[i].volumeInfo.description,
-                        link: items[i].saleInfo.buyLink,
-
+                for(var i=0; i < items.length; i++){
+                    if(!(items[i].volumeInfo.authors)){
+                        items[i].volumeInfo.authors=["unknown"]
                     }
-                )
+
+                    if(!(items[i].volumeInfo.imageLinks)){
+                        items[i].volumeInfo.imageLinks={};
+                        items[i].volumeInfo.imageLinks.thumbnail="../placeholder.png";
+                    }
+                }
+
+                for(var i=0; i < items.length; i++){
+                    books.push(
+                        {
+                            title: items[i].volumeInfo.title,
+                            author: items[i].volumeInfo.authors[0],
+                            img: items[i].volumeInfo.imageLinks.thumbnail,
+                            description: items[i].volumeInfo.description,
+                            link: items[i].saleInfo.buyLink,
+
+                        }
+                    )
+                }
+
+                let first = inputText[0].toUpperCase();
+                let info = $("<p></p>").text(first+inputText.substring(1,inputText.length)+" books");
+                info.addClass('book-info');
+                $("#info").append(info);
+
+                for(var i=0; i < books.length; i++){
+                    let title = $("<p></p>").text(books[i].title);
+                    title.addClass("book-title")
+                    let img = $("<img/>").attr("src",books[i].img);
+                    let author = $("<p></p>").text(books[i].author);
+
+                    let div = $("<div></div>");
+                    div.addClass("book");
+                    div.append(img);
+                    div.append(title);
+                    div.append(author);
+
+                    let link = $("<a></a>").attr("href",books[i].link);
+                    link.attr("target","_blank");
+                    link.append(div);
+                    booksContainer.append(link);
+                }
+                
+
+                console.log(books);
             }
-
-            
-            for(var i=0; i < books.length; i++){
-                let title = $("<p></p>").text(books[i].title);
-                title.addClass("book-title")
-                let img = $("<img/>").attr("src",books[i].img);
-                let author = $("<p></p>").text(books[i].author);
-
-                let div = $("<div></div>");
-                div.addClass("book");
-                div.append(img);
-                div.append(title);
-                div.append(author);
-
-                let link = $("<a></a>").attr("href",books[i].link);
-                link.attr("target","_blank");
-                link.append(div);
-                booksContainer.append(link);
-            }
-            
-
-            console.log(books);
         });
+            
     }else{
         alert("You need to type something");
     }
